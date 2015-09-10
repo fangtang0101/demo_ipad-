dc6ef1affe2779cef63d8f7bdb97e88aa62227d7
fangtang0101 <fang623003648>
2015-09-10 13:52:48 +0800

    Initial Commit

---
+++ demo_ipad导航栏/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  demo_ipad导航栏
+//
+//  Created by 方春高 on 15/9/10.
+//  Copyright (c) 2015年 方春高. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ demo_ipad导航栏/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  demo_ipad导航栏
+//
+//  Created by 方春高 on 15/9/10.
+//  Copyright (c) 2015年 方春高. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
